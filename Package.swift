081d9394f7d0aabd93368960b5902f6c9e229c97
greeshmay <greeshmay@gmail.com>
2023-08-25 20:02:18 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,12 +6,12 @@ let package = Package(
     products: [
         .library(
             name: "whisper-ios",
-            targets: ["whisper-ios"])
+            targets: ["whisper"])
     ],
     targets: [
-        .target(name: "whisper-ios", dependencies:[], cSettings: [.unsafeFlags(["-O3", "-DGGML_USE_ACCELERATE=1", "-DWHISPER_USE_COREML", "-DWHISPER_COREML_ALLOW_FALLBACK"])]),
-        .target(name: "test-objc", dependencies:["whisper-ios"]),
-        .target(name: "test-swift", dependencies:["whisper-ios"])
+        .target(name: "whisper", dependencies:[], cSettings: [.unsafeFlags(["-O3", "-DGGML_USE_ACCELERATE=1", "-DWHISPER_USE_COREML", "-DWHISPER_COREML_ALLOW_FALLBACK"])]),
+        .target(name: "test-objc", dependencies:["whisper"]),
+        .target(name: "test-swift", dependencies:["whisper"])
     ],
     cxxLanguageStandard: CXXLanguageStandard.cxx11
 )
